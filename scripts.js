0896b1af08a274c98f8db2113db3ef27b91175c2
Jaehyun(Jay) Shim <jaehyunshim85@gmail.com>
2022-05-15 01:19:46 -0600

    modifying and recreating the slide menu using js instead of css

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// sliding-menu
+const menuLogo = document.querySelector('.slidingMenu');
+const menuBar = document.querySelector('.slidingMenuBar');
+function menuClick(event) {
+  event.target.classList.toggle('clickedMenu');
+  menuBar.classList.toggle('clickedMenuBar');
+}
+menuLogo.addEventListener('click', menuClick);
+
+// 
